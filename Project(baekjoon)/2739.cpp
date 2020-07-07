336c5c2d1084fade9ecbafa29fb176c92a399a0d
Hjs <wltjd702@naver.com>
2020-07-07 17:09:47 +0900

    구구단

---
+++ Project(baekjoon)/2739.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio;
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i < 10; i++) {
+		cout << n << ' ' <<"*" <<' '<<i << ' '<< "=" <<' ' <<n * i<<"\n";
+	}
+
+	return 0;
+}
